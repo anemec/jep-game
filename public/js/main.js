ad03fd078cece0b70ea44fbacd42d8a4c45c67d1
Andrew Nemec <andrewnemec2@gmail.com>
2020-07-26 11:15:27 -0500

    AN - added logic to display questions

--- public/js/main.js
+++ public/js/main.js
@@ -1,35 +1,69 @@
 const testElem = document.getElementById('test');
 const questionBtn = document.getElementById('newQ');
-const questionDiv = document.getElementById('question');
-const alertBtn = document.getElementById('answer');
+const questionOne = document.getElementById('question1');
+const questionTwo = document.getElementById('question2');
+const questionThree = document.getElementById('question3');
+const questionFour = document.getElementById('question4');
+const questionFive = document.getElementById('question5');
+const answerBtn = document.getElementById('answer');
 
 const socket = io();
 
-async function getNewQuestion() {
-    let url = 'https://jservice.io/api/random'
-    // let response = await fetch(url);
+let questionsArr = [];
+// let questionsArr = ["This is an answer one", "This is an answer two", "This is an answer three", "This is an answer four", "This is an answer five"];
 
-    // let data = await response.json();
-    // console.log(data[0]);
-    // let realData = data[0];
-    fetch(url)
+async function getNewQuestion() {
+    let urlRandom = 'https://jservice.io/api/random';
+    let urlCat = 'https://jservice.io/api/category?id=2155';
+
+    fetch(urlCat)
     .then(response => {
         return response.json()
     })
     .then(data => { 
-        console.log(data[0]);
-        questionDiv.innerText = data[0].question;
+        console.log(data);
+        for (var i = 0; i < 5; i++) {
+            questionsArr[i] = data.clues[i];
+        }
+        // questionDiv.innerText = data[0].question;
     });
 
     // questionDiv.innerText = realData.question;
     // return realData.question;
 }
 
+// Question buttons
+questionOne.addEventListener('click', () => {
+    const ans = document.getElementById("answer1");
+    ans.innerText = questionsArr[0].question;
+});
+
+questionTwo.addEventListener('click', () => {
+    const ans = document.getElementById("answer2");
+    ans.innerText = questionsArr[1].question;
+});
+
+questionThree.addEventListener('click', () => {
+    const ans = document.getElementById("answer3");
+    ans.innerText = questionsArr[2].question;
+});
+
+questionFour.addEventListener('click', () => {
+    const ans = document.getElementById("answer4");
+    ans.innerText = questionsArr[3].question;
+});
+
+questionFive.addEventListener('click', () => {
+    const ans = document.getElementById("answer5");
+    ans.innerText = questionsArr[4].question;
+});
+
+// TODO update to change all questions
 questionBtn.addEventListener('click', () => {
     getNewQuestion();
 });
 
-alertBtn.addEventListener('click', () => {
+answerBtn.addEventListener('click', () => {
     socket.emit('answer', 'Answer button pressed');
 });
 
